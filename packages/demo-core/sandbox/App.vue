9458dd27b9251ac9f508659cd6030c58b3a0c468
Adam DeHaven <adam.dehaven@konghq.com>
2022-09-29 15:15:00 -0400

    fix: remove imports

--- packages/demo-core/sandbox/App.vue
+++ packages/demo-core/sandbox/App.vue
@@ -9,39 +9,19 @@
         <p>The component has loaded!</p>
         <kong-auth-login />
       </div>
-      <hr>
-      <TestButton
-        msg="Test"
-        :the-button-is-visible="true"
-      />
-      <!-- <NewComponent msg="This is the button" /> -->
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import { usePackage } from '../src'
-import { TestButton } from '@kong-ui/demo-component'
-
-// const ButtonComponent: typeof TestButton = TestButton
-
-// console.log(ButtonComponent)
 
 const { importPackage, loadingPackage, loadingStyles } = usePackage()
 // @ts-ignore
 // const NewComponent = window.kongui.demo_component.TestButton
 
-// let TestButton: any
-
-// const TestButton = defineAsyncComponent({
-//   // @ts-ignore
-//   // loader: () => import('https://packages.konghq.tech/@kong-ui/demo-component@0.2.1/dist/kong-ui.demo-component.es.js'),
-//   loader: () => import('@kong-ui/demo-component'),
-//   timeout: 30000,
-// })
-
 importPackage({
-  packageUrls: ['https://packages.konghq.tech/@kong/kong-auth-elements@1/dist/kong-auth-elements.es.js'],
+  scripts: ['https://packages.konghq.tech/@kong/kong-auth-elements@1/dist/kong-auth-elements.es.js'],
   styles: {
     urls: ['https://packages.konghq.tech/@kong/kong-auth-elements@1/dist/style.css'],
     shadowRoot: 'kong-auth-login',
@@ -55,7 +35,7 @@ importPackage({
 })
 
 // importPackage({
-//   packageUrls: ['https://packages.konghq.tech/@kong-ui/demo-component@0/dist/demo-component.es.js'],
+//   scripts: ['https://packages.konghq.tech/@kong-ui/demo-component@0/dist/demo-component.es.js'],
 //   styles: {
 //     urls: ['https://packages.konghq.tech/@kong-ui/demo-component@0/dist/style.css'],
 //   },
