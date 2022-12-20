6ecaf9f8a1df9081933fc6c2515eb861c1a34a8f
ValeryG <vvalggor@gmail.com>
2022-12-20 12:33:04 -0700

    chore: configuring proxy redirects for konnect-app-shell sandbox [KHCP-5656] (#304)
    * chore: configuring proxy redirects for konnect-app-shell sandbox [KHCP-5656]
    
    * chore: mesh-manager local. update reg-exp to cover future geos
    
    Co-authored-by: Val Gorodnichev <val.gorodnichev@konghq.com>

--- packages/konnect-app-shell/vite.config.ts
+++ packages/konnect-app-shell/vite.config.ts
@@ -16,6 +16,30 @@ const config = mergeConfig(sharedViteConfig, defineConfig({
       fileName: (format) => `${packageName}.${format}.js`,
     },
   },
+  base: '/',
+  server: {
+    proxy: {
+      // TODO when KHCP-5497 consume this from the helper function
+      '^/(?!([a-z]{2}/)?(index.ts|App.vue|router.ts|pages|sandbox|mesh-manager|src|node_modules|@vite|@id|@fs))': {
+        // TODO when PR-3589 merged to main in khcp-ui this needs to be replaced to https://cloud.konghq.tech/
+        target: 'https://pr-deploy-preview-3589--konnect-client-dev.netlify.app/',
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost',
+        // TODO: remove commented debugging
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err)
+          })
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url)
+          })
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+          })
+        },
+      },
+    },
+  },
 }))
 
 // If we are trying to preview a build of the local `package/konnect-app-shell/sandbox` directory,
